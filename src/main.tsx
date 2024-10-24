caeff820f198bbddeb647839714187b621959a9d
binh.nguyen <jackjack2000.kahp@gmail.com>
2024-10-24 13:33:41 +0700

    add RESTful api

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React, { StrictMode } from "react";
 import { Route } from "react-router-dom";
 import { App, ZMPRouter, AnimationRoutes, SnackbarProvider } from "zmp-ui";
 import { RecoilRoot } from "recoil";
-import HomePage from "pages/home/UIHome";
-import About from "pages/about";
-import Form from "pages/form";
-import User from "pages/user";
+import { UIHomePage } from "./pages/home/UIHome";
+import { UIFamilyTree } from "./pages/family-tree/UIFamilyTree";
 
 export function MyApp() {
   return (
@@ -15,10 +13,8 @@ export function MyApp() {
           <SnackbarProvider>
             <ZMPRouter>
               <AnimationRoutes>
-                <Route path="/" element={<HomePage></HomePage>}></Route>
-                {/* <Route path="/about" element={<About></About>}></Route>
-                <Route path="/form" element={<Form></Form>}></Route>
-                <Route path="/user" element={<User></User>}></Route> */}
+                <Route path="/" element={<UIHomePage/>}/>
+                <Route path="/ftree" element={<UIFamilyTree/>}/>
               </AnimationRoutes>
             </ZMPRouter>
           </SnackbarProvider>
--- src/pages/home/UIHome.tsx
+++ src/pages/home/UIHome.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Page, Icon, useNavigate, BottomNavigation } from "zmp-ui";
+import { Page, useNavigate, Grid } from "zmp-ui";
 
-const HomePage: React.FunctionComponent = () => {
+export function UIHomePage() {
   const navigate = useNavigate();
-  const [activeTab, setActiveTab] = React.useState("ftree");
 
   const handleTabChange = (key: string) => {
     navigate("/" + key);
@@ -11,41 +10,7 @@ const HomePage: React.FunctionComponent = () => {
 
   return (
     <Page className="page">
-      <BottomNavigation fixed activeKey={activeTab} onChange={(key) => setActiveTab(key)}>
-        <BottomNavigation.Item
-          key="ftree"
-          label="Family Tree"
-          icon={<Icon icon="zi-members" />}
-          activeIcon={<Icon icon="zi-members-solid" />}
-          onClick={() => handleTabChange("ftree")}
-        />
-        <BottomNavigation.Item
-          label="Danh bạ"
-          key="contact"
-          icon={<Icon icon="zi-call" />}
-          activeIcon={<Icon icon="zi-call-solid" />}
-        />
-        <BottomNavigation.Item
-          label="Khám phá"
-          key="discovery"
-          icon={<Icon icon="zi-more-grid" />}
-          activeIcon={<Icon icon="zi-more-grid-solid" />}
-        />
-        <BottomNavigation.Item
-          key="timeline"
-          label="Nhật ký"
-          icon={<Icon icon="zi-clock-1" />}
-          activeIcon={<Icon icon="zi-clock-1-solid" />}
-        />
-        <BottomNavigation.Item
-          key="me"
-          label="Cá nhân"
-          icon={<Icon icon="zi-user" />}
-          activeIcon={<Icon icon="zi-user-solid" />}
-        />
-      </BottomNavigation>
+      
     </Page>
   );
 };
-
-export default HomePage;
