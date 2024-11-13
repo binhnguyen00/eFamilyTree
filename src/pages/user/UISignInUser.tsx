495bd278dcad9dcffc7fe8b7c222a2abe7d74851
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-11-13 16:56:59 +0700

    setup sign in UI

--- src/pages/user/UISignInUser.tsx
+++ src/pages/user/UISignInUser.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Button, Stack } from "zmp-ui";
 
 export default function UISignInUser() {
   return (
-    <div>UILoginHome</div>
+    <Stack space="1rem">
+      <Button>
+        đăng nhập
+      </Button>
+      <Button>
+        đăng kí tài khoản
+      </Button>
+      <Button>
+        đăng kí dòng họ
+      </Button>
+    </Stack>
   )
 }
