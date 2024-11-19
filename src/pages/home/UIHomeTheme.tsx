7d4a016d32ac7bef266394f8754ab357be0ceebe
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-11-19 13:15:58 +0700

    refacor code

--- src/pages/home/UIHomeTheme.tsx
+++ src/pages/home/UIHomeTheme.tsx
@@ -1,78 +1,28 @@
 import React from "react";
 import { t } from "i18next";
-import { Box, Stack, Text } from "zmp-ui";
+import { Box, Stack, Text, useNavigate } from "zmp-ui";
 
-import themeRed from "assets/img/theme/theme-red.jpeg";
-import themeGreen from "assets/img/theme/theme-green.jpeg";
-import themeBlue from "assets/img/theme/theme-blue.jpeg";
-
-import { useTheme } from "hooks";
-import { CommonIcon, SizedBox } from "components";
+import { CommonIcon } from "components";
+import { UIThemeList } from "pages/theme/UITheme";
 
 export default function UIHomeTheme() {
+  const navigate = useNavigate();
 
   return (
     <Stack space="0.5rem">
 
-      <Box flex flexDirection="row" justifyContent="space-between">
+      <Box flex flexDirection="row" justifyContent="space-between" className="button" onClick={() => navigate("/theme")}>
         <Text.Title size="xLarge" className="text-capitalize"> {t("theme")} </Text.Title>
-        <Box flex flexDirection="row" alignItems="center" className="button">
+        <Box flex flexDirection="row" alignItems="center">
           <Text size="small"> {t("more")} </Text>
           <CommonIcon.ChevonRight size={"1rem"}/>
         </Box>
       </Box>
 
-      <UIThemeList/>
+      <div className="scroll-h flex-h">
+        <UIThemeList/>
+      </div>
 
     </Stack>
-  )
-}
-
-function UIThemeList() {
-  const { toggleTheme } = useTheme();
-
-  return (
-    <div className="scroll-h flex-h">
-
-      <Stack space="0.5rem" className="center text-capitalize">
-        <SizedBox 
-          className="button"
-          width={150} 
-          height={100} 
-          border
-          onClick={() => toggleTheme("default")}
-        >
-          <img src={themeRed} alt="theme red"/>
-        </SizedBox>
-        <Text> {t("theme_red")} </Text>
-      </Stack>
-
-      <Stack space="0.5rem" className="center text-capitalize">
-        <SizedBox 
-          className="button"
-          width={150} 
-          height={100} 
-          border
-          onClick={() => toggleTheme("blue")}
-        >
-          <img src={themeBlue} alt="theme blue"/>
-        </SizedBox>
-        <Text> {t("theme_blue")} </Text>
-      </Stack>
-
-      <Stack space="0.5rem" className="center text-capitalize">
-        <SizedBox 
-          className="button"
-          width={150} 
-          height={100} 
-          border
-          onClick={() => toggleTheme("green")}
-        >
-          <img src={themeGreen} alt="theme green"/>
-        </SizedBox>
-        <Text> {t("theme_green")} </Text>
-      </Stack>
-
-    </div>
   )
 }
--- src/pages/theme/UITheme.tsx
+++ src/pages/theme/UITheme.tsx
@@ -1,27 +1,70 @@
 import React from "react";
 import { t } from "i18next";
 
-import { Grid } from "zmp-ui";
+import { Grid, Stack, Text } from "zmp-ui";
 
 import {  useTheme } from "hooks";
-import { Header } from "components";
+import { Header, SizedBox } from "components";
+
+import themeRed from "assets/img/theme/theme-red.jpeg";
+import themeGreen from "assets/img/theme/theme-green.jpeg";
+import themeBlue from "assets/img/theme/theme-blue.jpeg";
 
 export default function UITheme() {
   return (
     <div className="container">
       <Header title={t("theme")}/>
 
-      <UIThemeSelector/>
+      <Grid columnSpace="1rem" rowSpace="1rem" columnCount={2}>
+        <UIThemeList/>
+      </Grid>
     </div>
   )
 }
 
-function UIThemeSelector() {
+export function UIThemeList() {
   const { toggleTheme } = useTheme();
 
   return (
-    <Grid columnSpace="1rem" rowSpace="1rem" columnCount={2}>
-      TODO
-    </Grid>
+    <>
+      <Stack space="0.5rem" className="center text-capitalize">
+        <SizedBox 
+          className="button"
+          width={150} 
+          height={100} 
+          border
+          onClick={() => toggleTheme("default")}
+        >
+          <img src={themeRed} alt="theme red"/>
+        </SizedBox>
+        <Text> {t("theme_red")} </Text>
+      </Stack>
+
+      <Stack space="0.5rem" className="center text-capitalize">
+        <SizedBox 
+          className="button"
+          width={150} 
+          height={100} 
+          border
+          onClick={() => toggleTheme("blue")}
+        >
+          <img src={themeBlue} alt="theme blue"/>
+        </SizedBox>
+        <Text> {t("theme_blue")} </Text>
+      </Stack>
+
+      <Stack space="0.5rem" className="center text-capitalize">
+        <SizedBox 
+          className="button"
+          width={150} 
+          height={100} 
+          border
+          onClick={() => toggleTheme("green")}
+        >
+          <img src={themeGreen} alt="theme green"/>
+        </SizedBox>
+        <Text> {t("theme_green")} </Text>
+      </Stack>
+    </>
   )
 }
