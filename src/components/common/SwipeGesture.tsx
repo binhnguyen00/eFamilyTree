036c2d269f12558b762cc876af2166867c04ab8a
marci <jackjack2000.kahp@gmail.com>
2024-11-24 14:46:12 +0700

    fix bug

--- src/components/common/SwipeGesture.tsx
+++ src/components/common/SwipeGesture.tsx
@@ -3,7 +3,7 @@ import { useSwipeable } from 'react-swipeable';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'zmp-ui';
 
-export function SwipeGesture({ children }: { children: React.ReactNode }) {
+export default function SwipeGesture({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
   const location = useLocation();
 
