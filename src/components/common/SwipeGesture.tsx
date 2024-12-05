d47c077e8fca1b9f7eb5a5d482a4b5fd60033db3
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-05 10:51:05 +0700

    add font

--- src/components/common/SwipeGesture.tsx
+++ src/components/common/SwipeGesture.tsx
@@ -5,6 +5,10 @@ import { useNavigate } from 'zmp-ui';
 import { useRecoilValue } from 'recoil';
 import { swipeDisabledPathsAtom } from 'states';
 
+/**
+ * @PageDisable Disable swipe gesture on some path
+ * @SwipeRange Can be swiped only on the left side of the screen. Swipe range is about 30px
+ */
 export default function SwipeGesture({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
   const location = useLocation();
@@ -12,13 +16,24 @@ export default function SwipeGesture({ children }: { children: React.ReactNode }
   const isSwipeDisabled = swipeDisabledPaths.includes(location.pathname);
 
   const handlers = useSwipeable({
-    onSwipedRight: () => {
-      if (!isSwipeDisabled && location.pathname !== '/') {
+    onSwipedRight: (eventData) => {
+      if (!isSwipeDisabled && location.pathname !== '/' && eventData.initial[0] <= 30) {
         navigate(-1);
       }
     },
     onTouchStartOrOnMouseDown: (({ event }) => {
       if (isSwipeDisabled) event.preventDefault();
+      let clientX: number | undefined;
+      // Handle TouchEvent
+      if ((event as TouchEvent).changedTouches) {
+        clientX = (event as TouchEvent).changedTouches[0].clientX;
+      }
+      // Handle MouseEvent
+      else {
+        clientX = (event as MouseEvent).clientX;
+      }
+      // Restrict gesture to the first 30px area
+      if (clientX && clientX > 30) return;
     }),
     preventScrollOnSwipe: true,
     trackTouch: true,
