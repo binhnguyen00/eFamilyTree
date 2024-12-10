e7db35baa3a571dba68bbf526cd457e7d4f9ae6c
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-10 08:22:29 +0700

    update banner

--- src/pages/home/UIHomeBanner.tsx
+++ src/pages/home/UIHomeBanner.tsx
@@ -1,18 +1,42 @@
 import React from "react";
 import { t } from "i18next";
-import { Stack, Swiper, Text } from "zmp-ui";
+import { Box, Button, Stack, Swiper, Text, useNavigate } from "zmp-ui";
 import { ImageWithText } from "components";
 
 import banner_01 from "assets/img/banner/banner-01.jpg";
 import banner_02 from "assets/img/banner/banner-02.jpg";
 import banner_03 from "assets/img/banner/banner-03.jpg";
+import banner_04 from "assets/img/banner/banner-04.jpg";
 
 export function UIHomeBanner() {
+  const navigate = useNavigate();
+
+  const onRegister = () => {
+    navigate("/register");
+  }
 
   return (
     <Stack space="0.5rem">
       <Text.Title size="xLarge"> {t("family_tree")} </Text.Title>
       <Swiper loop autoplay>
+        <Swiper.Slide>
+          <React.Suspense>
+            <ImageWithText 
+              text={
+                <Stack space="1rem">
+                  <p>{"Lan toả tới"}</p>
+                  <p>{"Những người yêu thương"}</p>
+                  <Box flex flexDirection="row" justifyContent="center">
+                    <Button variant="primary" size="small" onClick={onRegister}>  {t("register")} </Button>
+                  </Box>
+                </Stack>
+              }
+              textStyle={{ fontSize: "1.5rem" }}
+              src={banner_04}
+              height={150}
+            />
+          </React.Suspense>
+        </Swiper.Slide>
         <Swiper.Slide>
           <React.Suspense>
             <ImageWithText 
