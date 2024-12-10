0346b8c95b38558b13b4aa0dd7e24f252ffb697b
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-10 17:06:49 +0700

    add SwipeGestureNew.tsx

--- src/components/common/SwipeGesture.tsx
+++ src/components/common/SwipeGesture.tsx
@@ -22,7 +22,6 @@ export function SwipeGesture({ children }: { children: React.ReactNode }) {
       }
     },
     onTouchStartOrOnMouseDown: (({ event }) => {
-      // if (isSwipeDisabled) event.preventDefault();
       let clientX: number | undefined;
       // Handle TouchEvent
       if ((event as TouchEvent).changedTouches) {
