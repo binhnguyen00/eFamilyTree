4f94b1f4c097dc86e779651782e17421872f377f
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-10 09:50:42 +0700

    compress image

--- src/pages/home/UIHomeBanner.tsx
+++ src/pages/home/UIHomeBanner.tsx
@@ -18,7 +18,7 @@ export function UIHomeBanner() {
   return (
     <Stack space="0.5rem">
       <Text.Title size="xLarge" className="text-capitalize"> {t("family_tree")} </Text.Title>
-      <Swiper loop autoplay>
+      <Swiper>
         <Swiper.Slide>
           <React.Suspense>
             <ImageWithText 
