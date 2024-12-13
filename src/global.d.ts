f256dda9b039dbd2f475ed3acd4f0222bf31d4e4
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-13 10:54:24 +0700

    idk

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,16 @@ declare module "*.png" {
   export default content;
 }
 
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
 declare module "*.svg" {
   const content: string;
   export default content;
