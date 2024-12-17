4c7a96f4157c0931332a3064555b9417cc361a01
binhnguyen00 <jackjack2000.kahp@gmail.com>
2024-12-17 14:27:44 +0700

    add settings api

--- src/api/UserSettingApi.ts
+++ src/api/UserSettingApi.ts
@@ -10,4 +10,42 @@ export class UserSettingApi extends BaseServer {
     })
     return this.server.POST("account/setting", header, body, successCB, failCB);
   }
+
+  public static getDefault(successCB: SuccessCB, failCB?: FailCB) {
+    const header = this.initHeader();
+    return this.server.POST("account/setting/default", header, null, successCB, failCB);
+  }
+
+  public static updateOrCreate(
+    phoneNumber: string, 
+    theme: string,
+    language: string,
+    successCB: SuccessCB, 
+    failCB?: FailCB
+  ) {
+    const header = this.initHeader();
+    const body = this.initBody({
+      phone_number: phoneNumber,
+      theme: theme,
+      language: language,
+    })
+    this.server.POST("account/setting/save", header, body, successCB, failCB);
+  }
+
+  public static getBackground(phoneNumber: string, successCB: SuccessCB, failCB?: FailCB) {
+    const header = this.initHeader();
+    const body = this.initBody({
+      phone_number: phoneNumber
+    })
+    this.server.POST("account/setting/background", header, body, successCB, failCB);
+  }
+
+  public static updateBackground(phoneNumber: string, background: FormData, successCB: SuccessCB, failCB?: FailCB) {
+    const header = this.initHeader();
+    const body = this.initBody({
+      phone_number: phoneNumber,
+      background: background
+    })
+    this.server.POST("account/setting/background/save", header, body, successCB, failCB);
+  }
 }
