c46edf4d97e2bf076f3972363c74da1b721ccdb3
binhnguyen00 <jackjack2000.kahp@gmail.com>
2025-01-03 17:02:53 +0700

    add scrollable component

---
+++ src/components/common/ScrollableDiv.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface ScrollableDivProps {
+  children: React.ReactNode;
+  width?: number | string | "auto";
+  height?: number | string | "auto";
+  direction?: "horizontal" | "vertical" | "both";
+  className?: string;
+}
+
+export function ScrollableDiv(props: ScrollableDivProps) {
+  let { width = "auto", height = "auto", direction = "horizontal", children, className } = props; 
+
+  const getScrollStyles = (direction: "horizontal" | "vertical" | "both") => {
+    switch (direction) {
+      case 'vertical':
+        return { overflowY: 'auto', overflowX: 'hidden' };
+      case 'horizontal':
+        return { overflowX: 'auto', overflowY: 'hidden' };
+      default:
+        return { overflow: 'auto' };
+    }
+  };
+
+  const scrollableStyle = {
+    width: width,
+    height: height,
+    ...getScrollStyles(direction),
+  } as React.CSSProperties;
+
+  return (
+    <div 
+      style={scrollableStyle}
+      className={`${className ? className : ''}`}
+    >
+      {children}
+    </div>
+  )
+}
