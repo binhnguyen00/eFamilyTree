cf6ea6af9ec6e57e9197a2fde7ebbafb0af4a8c3
binhnguyen00 <jackjack2000.kahp@gmail.com>
2025-01-08 16:01:37 +0700

    Tree now zoom to root on first open

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Box } from 'zmp-ui';
 
-import Connector from './TreeConnector';
 import calcTree from 'components/tree-relatives';
 import { Gender, Node } from 'components/tree-relatives/types';
+
 import { useAppContext } from 'hooks';
+import { CommonUtils } from 'utils';
+
+import Connector from './TreeConnector';
 import { TreeConfig } from './TreeConfig';
 import { TreeSearchBar } from './TreeSearchBar';
 import { TreeController } from './TreeController';
 
 import "./css/transform-wrapper.scss";
-import { CommonUtils } from 'utils';
 
 interface TreeProps {
   nodes: Node[];
@@ -55,46 +57,8 @@ export default React.memo<TreeProps>(function Tree(props) {
   const nodeHeight = props.nodeHeight / 2;
   const treeWidth = data.canvas.width * nodeWidth;
   const treeHeight = data.canvas.height * nodeHeight;
-
-  let { getTreeBackgroundPath } = useAppContext();
-  let treeRef = React.useRef<HTMLDivElement | null>(null);
-  const tree = () => {
-    let treeBgImgPath = getTreeBackgroundPath();
-    let background = {} as React.CSSProperties;
-    if (!CommonUtils.isStringEmpty(treeBgImgPath)) {
-      background = {
-        backgroundImage: `url(${treeBgImgPath})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      };
-    } else background = {
-      backgroundColor: `var(--tree-background-color)`,
-    }
-
-    return (
-      <div
-        id="tree-canvas"
-        ref={treeRef}
-        className={`border-primary ${props.className ? props.className : ""}`}
-        style={{
-          width: treeWidth,
-          height: treeHeight,
-          borderRadius: "2rem",
-          ...background,
-        }}
-      >
-        {data.connectors.map((connector, idx) => (
-          <Connector
-            key={idx}
-            connector={connector}
-            width={nodeWidth}
-            height={nodeHeight}
-          />
-        ))}
-        {data.nodes.map(props.renderNode)}
-      </div>
-    )
-  }
+  const treeRef = React.useRef<HTMLDivElement | null>(null);
+  const { getTreeBackgroundPath } = useAppContext();
 
   return (
     <div 
@@ -109,34 +73,129 @@ export default React.memo<TreeProps>(function Tree(props) {
         centerOnInit
         centerZoomedOut
       >
-        {({ zoomIn, zoomOut, zoomToElement }) => (
-          <React.Fragment>
-            <Box flex flexDirection='row' justifyContent='space-between'>
-              <TreeSearchBar 
-                searchFields={searchFields}
-                displayField={props.searchDisplayField}
-                nodes={props.nodes}
-                onSelect={zoomToElement}
-              />
-              <TreeController
-                rootId={rootId}
-                onZoomIn={zoomIn}
-                onZoomOut={zoomOut}
-                onZoomToRoot={zoomToElement}
-                html2export={{
-                  content: tree(),
-                  width: treeWidth,
-                  height: treeHeight,
-                }}
-                onReset={props.onReset}
-              />
-            </Box>
-            <TransformComponent>
-              {tree()}
-            </TransformComponent>
-          </React.Fragment>
-        )}
+        {({ zoomIn, zoomOut, zoomToElement }) => {
+          return (
+            <React.Fragment>
+              <Box flex flexDirection='row' justifyContent='space-between'>
+                <TreeSearchBar 
+                  searchFields={searchFields}
+                  displayField={props.searchDisplayField}
+                  nodes={props.nodes}
+                  onSelect={zoomToElement}
+                />
+                <TreeController
+                  rootId={rootId}
+                  onZoomIn={zoomIn}
+                  onZoomOut={zoomOut}
+                  onZoomToRoot={zoomToElement}
+                  html2export={{
+                    content: (                
+                      <TreeContainer 
+                        rootId={rootId}
+                        treeRef={treeRef}
+                        nodeWidth={nodeWidth}
+                        nodeHeight={nodeHeight}
+                        calculatedData={data}
+                        renderNode={props.renderNode}
+                        backgroundPath={getTreeBackgroundPath()}
+                      />
+                    ),
+                    width: treeWidth,
+                    height: treeHeight,
+                  }}
+                  onReset={props.onReset}
+                />
+              </Box>
+              <TransformComponent>
+                <TreeContainer 
+                  rootId={rootId}
+                  treeRef={treeRef}
+                  nodeWidth={nodeWidth}
+                  nodeHeight={nodeHeight}
+                  calculatedData={data}
+                  renderNode={props.renderNode}
+                  zoomToRoot={zoomToElement}
+                  backgroundPath={getTreeBackgroundPath()}
+                />
+              </TransformComponent>
+            </React.Fragment>
+          )
+        }}
       </TransformWrapper>
     </div>
   );
 });
+
+interface TreeContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  rootId: string;
+  nodeWidth: number;
+  nodeHeight: number;
+  calculatedData: any;
+  treeRef: any;
+  backgroundPath: string;
+  renderNode: (node: any) => React.ReactNode;
+  zoomToRoot?: (root: HTMLElement, scale?: number) => void;
+}
+function TreeContainer(props: TreeContainerProps) {
+  const { 
+    calculatedData, treeRef, nodeHeight, nodeWidth, backgroundPath, rootId,
+    renderNode, zoomToRoot,
+  } = props;
+
+  const handleBackground = () => {
+    let background = {
+      backgroundColor: `var(--tree-background-color)`
+    } as React.CSSProperties;
+    if (!CommonUtils.isStringEmpty(backgroundPath)) {
+      background = {
+        backgroundImage: `url(${backgroundPath})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      };
+    }
+    return background;
+  }
+
+  const treeWidth = calculatedData.canvas.width * nodeWidth;
+  const treeHeight = calculatedData.canvas.height * nodeHeight;
+
+  React.useEffect(() => {
+    if (!treeRef.current || !zoomToRoot) return;
+    
+    const root = document.querySelector<HTMLDivElement>(`#node-${rootId}`);
+
+    const checkRender = () => {
+      if (root) {
+        zoomToRoot(root, 2);
+      } else {
+        requestAnimationFrame(checkRender); // Keep checking until rendered
+      }
+    };
+
+    requestAnimationFrame(checkRender);
+  }, [ ]);
+
+  return (
+    <div
+      id="tree-canvas"
+      ref={treeRef}
+      className={`border-primary ${props.className ? props.className : ""}`}
+      style={{
+        width: treeWidth,
+        height: treeHeight,
+        borderRadius: "2rem",
+        ...handleBackground(),
+      }}
+    >
+      {calculatedData.connectors.map((connector, idx) => (
+        <Connector
+          key={idx}
+          connector={connector}
+          width={nodeWidth}
+          height={nodeHeight}
+        />
+      ))}
+      {calculatedData.nodes.map(renderNode)}
+    </div>
+  )
+}
