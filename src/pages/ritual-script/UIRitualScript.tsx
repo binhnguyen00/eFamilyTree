652b02d73a39dbc32434df9680d1a3ca97f7f1ba
binhnguyen00 <jackjack2000.kahp@gmail.com>
2025-03-31 15:58:06 +0700

    set up vu lan script

--- src/pages/ritual-script/UIRitualScript.tsx
+++ src/pages/ritual-script/UIRitualScript.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { t } from "i18next";
-import { Button, Text } from "zmp-ui";
+import { Text } from "zmp-ui";
 
-import { CommonIcon, Header, ScrollableDiv, Selection } from "components";
+import { Header, ScrollableDiv, Selection, SelectionOption } from "components";
 
-import { UIAncestralOfferingForm } from "./ancestral-offering-letter/UIForm";
+import { UIGiaTienForm } from "./gia-tien-script/UIForm";
+import { UIVuLanForm } from "./vu-lan-script/UIForm";
 
 export function UIRitualScript() {
-  const [ scriptContainer, setContainer ] = React.useState<React.ReactNode>(
-    <UIAncestralOfferingForm/>
-  );
+  const [ number, setNumber ] = React.useState<number>(1);
 
   const options: any[] = [
-    { value: 1, label: "Lễ Gia Tiên" }
+    { value: 1, label: "Lễ Gia Tiên" },
+    { value: 2, label: "Vu Lan" },
   ]
+
+  const renderContainer = () => {
+    switch (number) { 
+      case 1: 
+        return <UIGiaTienForm />;
+      case 2: 
+        return <UIVuLanForm/>;
+      default: 
+        return <UIGiaTienForm />;
+    }
+  }
 
   return (
     <>
       <Header title={t("ritual_script")}/>
 
       <div className="container bg-white flex-v">
-
         <div>
           <Text.Title className="my-2 text-primary text-capitalize">
             {t("chọn loại sớ")}
@@ -28,18 +38,15 @@ export function UIRitualScript() {
           <Selection 
             options={options}
             defaultValue={options[0]}
-            isClearable={false}
-            isSearchable={false}
-            onChange={() => console.log("on change")} 
-            label={""} field={""} 
-            observer={null as any}          
+            onChange={(selected: SelectionOption, action) => setNumber(selected.value)} 
+            isClearable={false} isSearchable={false}
+            label={""} field={""} observer={null as any}
           />
         </div>
 
         <ScrollableDiv direction="vertical" className="flex-v">
-          {scriptContainer}
+          {renderContainer()}
         </ScrollableDiv>
-
       </div>
     </>
   )
