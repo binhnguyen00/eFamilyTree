00fe9dcca160ec47323e40cde4371cf9a35ce2e8
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-06-10 09:57:37 +0700

    enhance Toolbar

--- src/components/common/Toolbar.tsx
+++ src/components/common/Toolbar.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import classNames from "classnames";
 
 interface ToolbarProps {
   children: React.ReactNode;
+  boxShadow?: boolean;
+  glass?: boolean;
 }
 export function Toolbar(props: ToolbarProps) {
-  const { children } = props;
+  const { children, boxShadow = true, glass = true } = props;
 
   return (
-    <div className="absolute center bottom-12 flex-h scroll-h" style={{ zIndex: 999, width: "-webkit-fill-available" }}>
+    <div 
+      className={classNames(
+        "absolute bottom-6 left-1/2 -translate-x-1/2 rounded p-3 scroll-h", 
+        {
+          "shadow-lg": boxShadow,
+          "bg-white/10 backdrop-blur-md": glass,
+        })
+      }
+      style={{ 
+        zIndex: 999,
+        width: "95vw"
+      }}
+    >
       {children}
     </div>
   )
